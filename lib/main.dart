a23f8ae663eb2f4442050730ff2a9c9ee44b6f42
Ali Mohammadzadeh <ali24mohammadzadeh@gmail.com>
2023-04-06 14:11:48 +0430

    first commit to push to github repository

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculatorApplication());
+}
+
+class CalculatorApplication extends StatefulWidget {
+  const CalculatorApplication({super.key});
+
+  @override
+  State<CalculatorApplication> createState() => _CalculatorApplicationState();
+}
+
+class _CalculatorApplicationState extends State<CalculatorApplication> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
